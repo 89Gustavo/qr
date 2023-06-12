1f2d69098d13d00c430800af5e519027ac156347
gustavos.sicaja <4gsicaja.adolfo@gmail.com>
2023-06-12 11:09:17 -0600

    Acutualizaicon 12/06/2023, obtener registros a nivel de consola

--- qr_reader/lib/pages/home_page.dart
+++ qr_reader/lib/pages/home_page.dart
@@ -34,8 +34,9 @@ class _HomePageBody extends StatelessWidget {
     final uiProvider = Provider.of<UiProvidr>(context);
     final currentIndex = uiProvider.selectedMenuOpt;
     //DBProvider.db.database;
-    final temScan = ScanModel(valor: 'http://google.cualquiercosa.com');
-    DBProvider.db.nuevoScan(temScan);
+    //final temScan = ScanModel(valor: 'http://google.cualquiercosa.com');
+    //DBProvider.db.nuevoScan(temScan);
+    DBProvider.db.getTodosLosScans().then((scan) => print(scan));
     switch (currentIndex) {
       case 0:
         return const MapasPage();
